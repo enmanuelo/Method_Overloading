7d4b7e34d3bd2043f25840ea221365c29699b3af
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-11 13:08:05 -0400

    created 3 versions of Add() method

---
+++ Method_Overloading/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Method_Overloading
+{
+	public class Methods
+	{
+		public static int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		public static decimal Add(decimal a, decimal b)
+		{
+			return a + b;
+		}
+
+		public static string Add(int a, int b, bool c)
+		{         
+            if (c == true)
+            {
+				if (a + b != 1)
+                {
+					return $"{a + b} dollars";
+                }
+                else
+                {
+					return $"{a + b} dollar";
+
+				}
+            }
+            else
+            {
+				return "";
+            }
+		}
+
+	}
+}
+
---
+++ Method_Overloading/Program.cs
@@ -0,0 +1,10 @@
+﻿using Method_Overloading;
+
+var sumInteger = Methods.Add(2, 3);
+Console.WriteLine(sumInteger);
+
+var sumDecimal = Methods.Add(0.13m, 1.3m);
+Console.WriteLine(sumDecimal);
+
+var sumDollar = Methods.Add(2, 3, true);
+Console.WriteLine(sumDollar);
